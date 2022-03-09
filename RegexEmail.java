28574e7bef6c80f2a0f4797dcea364e9012fb021
Teju199 <tejuiyer95@gmail.com>
2022-03-09 22:36:09 +0530

    Program added with Email pattern.

--- RegexEmail.java
+++ RegexEmail.java
@@ -6,20 +6,19 @@ import java.util.regex.Pattern;
 
 public class RegexEmail {
     private static final String EMAIL_PATTERN =
-            ("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" +
-            "[^-][A-Za-z0-9-]+(\\.[A-Za-z]{3})+(\\.[A-Za-z0-9]+)*$");
+            "^[a-zA-Z0-9_*+-]+(?:\\.[a-zA-Z0-9_*+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
 
     private static Pattern p = Pattern.compile(EMAIL_PATTERN);
 
-    public static boolean validateEmail(String email) {
+    public static boolean validateEmail(String email) throws RegexEmailException {
         Matcher match = p.matcher(email);
         if (match.matches()) {
             return true;
-        }
-        return false;
+        }else
+            throw new RegexEmailException("Please enter valid email address.");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RegexEmailException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the email address");
         String email = sc.next();
