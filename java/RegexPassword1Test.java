a0e11955fd7cde8ae3c95de69227e0f616ac8bf9
Teju199 <tejuiyer95@gmail.com>
2022-03-09 22:50:57 +0530

    Program modified to give custom exception.

--- java/RegexPassword1Test.java
+++ java/RegexPassword1Test.java
@@ -1,19 +1,32 @@
+import MoodAnalyser1.MoodAnalysisException;
 import Regex.RegexPassword1;
+import Regex.RegexPasswordException;
 import junit.framework.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class RegexPassword1Test {
     @Test
-    public void GivenPassword_WhenProper_ReturnsTrue() {
+    public void GivenPassword_WhenProper_ReturnsTrue() throws RegexPasswordException {
         RegexPassword1 password1 = new RegexPassword1();
         boolean result = password1.validatePassword("welcome1");
         Assert.assertEquals(true, result);
     }
 
     @Test
-    public void GivenPassword_WhenProper_ReturnsFalse() {
+    public void GivenPassword_WhenNotProper_ReturnsMessage() throws RegexPasswordException {
         RegexPassword1 password2 = new RegexPassword1();
-        boolean result = password2.validatePassword("welcome_1");
-        Assert.assertEquals(false, result);
+
+        try{
+            //Exception Rule is created.
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(RegexPasswordException.class);
+            RegexPassword1.validatePassword("welcome_1");
+        }
+        catch(RegexPasswordException e){
+            e.printStackTrace();
+            Assert.assertEquals("Please enter valid password.", e);
+        }
+
     }
 }
